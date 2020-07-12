2656266f80bcd807ab17463db434d9cb3eca919c
Alice Zhao <azhao991@gmail.com>
2020-07-11 17:08:13 -0700

    Start branding my website

--- components/intro.js
+++ components/intro.js
@@ -2,19 +2,10 @@ import { CMS_NAME } from '../lib/constants'
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
+    <section className="flex-col md:flex-row flex items-center md:justify-between mb-16 mt-8 md:mb-12">
+      <img src="assets/logo.png" alt="Nibbles N Bytes logo" className="h-32 md:h-40 " />
+      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8 font-bold">
+        A journey from sales to code.
       </h4>
     </section>
   )

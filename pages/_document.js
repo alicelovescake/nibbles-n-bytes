85c49ac12d134e2053e0c40883399d66a7362911
Alice Zhao <azhao991@gmail.com>
2020-07-17 08:47:46 -0700

    add google metatag to verify ownership

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="google-site-verification" content="Eyxw8UOV5-nhHKNaVMmuR7Xminr9YR1NTbaXMqxLs8g" />
+        </Head>
         <body>
           <Main />
           <NextScript />
